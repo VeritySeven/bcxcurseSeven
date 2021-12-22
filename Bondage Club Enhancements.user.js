170d5d611c654b5c7a881ab1edabb93a11931d6a
VeritySeven <92721445+VeritySeven@users.noreply.github.com>
2021-12-21 20:11:13 -0600

    Update Bondage Club Enhancements.user.js

--- Bondage Club Enhancements.user.js
+++ Bondage Club Enhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Bondage Club Enhancements
 // @namespace https://www.bondageprojects.com/
-// @version 1.4.10
+// @version 1.4.11
 // @description enhancements for the bondage club
 // @author Sidious
 // @match https://bondageprojects.elementfx.com/*
@@ -14,7 +14,7 @@
 // @run-at document-end
 // ==/UserScript==
 
-window.BCE_VERSION = "1.4.10";
+window.BCE_VERSION = "1.4.11";
 
 (async function () {
   "use strict";
@@ -23,8 +23,7 @@ window.BCE_VERSION = "1.4.10";
   const DISCORD_INVITE_URL = "https://discord.gg/aCCWVzXBUj";
 
   const BCX_SOURCE =
-    "https://raw.githubusercontent.com/VeritySeven/bcxcurseSeven/b0cb6055e1a2fe3661a63908dc17b66869637d38/bcxedit.js";
-
+    "https://raw.githubusercontent.com/VeritySeven/bcxcurseSeven/437370c8f59b5ae07a7f45de639eab4f1c924886/bcxedit.js";
   const BCX_DEVEL_SOURCE =
     "https://jomshir98.github.io/bondage-club-extended/devel/bcx.js";
 
@@ -1056,6 +1055,10 @@ window.BCE_VERSION = "1.4.10";
     await fetch(source)
       .then((resp) => resp.text())
       .then((resp) => {
+        resp = resp.replace(
+          `sourceMappingURL=bcx.js.map`,
+          `sourceMappingURL=${source}.map`
+        );
         bce_log(resp);
         eval(resp);
       });
